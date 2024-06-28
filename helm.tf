080348b1bebd8a69f4bf4d05e604a61998aae1f1
nandreanurag <nandre.a@northeastern.edu>
2024-06-27 22:17:56 -0400

    fix: dynamically provisioned charts from bitnami artifactory

--- helm.tf
+++ helm.tf
@@ -1,29 +1,52 @@
 resource "helm_release" "kafka" {
   depends_on = [kubernetes_namespace.kafka]
   name       = "my-kafka"
-  chart      = "helm-charts/kafka"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "kafka"
   namespace  = kubernetes_namespace.kafka.metadata[0].name
+  set {
+    name  = "sasl.client.users[0]"
+    value = var.kafka_username
+  }
 
-  values = [
-    templatefile("helm-charts/kafka/values.yaml", {
-      kafka_password = var.kafka_password
-      kafka_username = var.kafka_username
-    })
-  ]
+  set {
+    name  = "sasl.client.passwords[0]"
+    value = var.kafka_password
+  }
+
+  set {
+    name  = "controller.resourcesPreset"
+    value = "medium"
+  }
 }
+
+
 
 resource "helm_release" "postgres" {
   depends_on = [kubernetes_namespace.postgres]
   name       = "my-postgres"
-  chart      = "helm-charts/postgresql"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "postgresql"
   namespace  = kubernetes_namespace.postgres.metadata[0].name
 
-  values = [
-    templatefile("helm-charts/postgresql/values.yaml", {
-      postgres_username = var.postgres_username
-      postgres_password = var.postgres_password
-      postgres_database = var.postgres_database
-      PGDUMP_DIR        = "/tmp"
-    })
-  ]
+  set {
+    name  = "global.postgresql.auth.username"
+    value = var.postgres_username
+  }
+  set {
+    name  = "global.postgresql.auth.postgresqlPassword"
+    value = var.postgres_password
+  }
+  set {
+    name  = "global.postgresql.auth.password"
+    value = var.postgres_password
+  }
+  set {
+    name  = "global.postgresql.auth.database"
+    value = var.postgres_database
+  }
+  set {
+    name  = "primary.resourcesPreset"
+    value = "small"
+  }
 }
