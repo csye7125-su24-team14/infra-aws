4b2e6a463d0309a4087f1f62dc5b511aea9e9423
nandreanurag <nandre.a@northeastern.edu>
2024-08-08 16:14:37 -0400

    fix: fmt changes

--- eks_addon.tf
+++ eks_addon.tf
@@ -1,13 +1,13 @@
 module "eks_blueprints_addons" {
-  source = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0" 
-  cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.cluster_endpoint
-  cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn
-  enable_external_dns                    = true
-  enable_cert_manager                    = true
-  cert_manager_route53_hosted_zone_arns  = ["arn:aws:route53:::hostedzone/${var.hostedZoneId}"]
+  source                                = "aws-ia/eks-blueprints-addons/aws"
+  version                               = "~> 1.0"
+  cluster_name                          = module.eks.cluster_name
+  cluster_endpoint                      = module.eks.cluster_endpoint
+  cluster_version                       = module.eks.cluster_version
+  oidc_provider_arn                     = module.eks.oidc_provider_arn
+  enable_external_dns                   = true
+  enable_cert_manager                   = true
+  cert_manager_route53_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${var.hostedZoneId}"]
 
   tags = {
     Environment = "cert-manager"
